eb01ec2e81613a19aa98e5ca0b194e5b776f93e4
jooho2075 <parkjh2075@icloud.com>
2026-04-02 17:02:13 +0900

    26.04.02.목:service패키지_IngestionService.java, RagService.java

--- src/main/java/dev/rag/service/IngestionService.java
+++ src/main/java/dev/rag/service/IngestionService.java
@@ -1,5 +1,38 @@
 package dev.rag.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
 // 실제 경험(경력기술서 등)을 Document 객체로 만들고, 이를 ChromaDB(Vector Store)ㄴ에 저장하는 복잡한 작업을 수행
+@Service
+@RequiredArgsConstructor
 public class IngestionService {
+    private final VectorStore vectorStore;
+
+    // resources/data 폴더의 .md 파일 가져오기
+    @Value("classpath:data/*.md")
+    private Resource[] resources;
+    public void ingestData() {
+        if(resources == null || resources.length == 0) {
+            System.out.println(".md 파일이 resources/data 위치에 없습니다...ㅠㅠ");
+            return;
+        }
+
+        for(Resource resource : resources) {
+            // 1.Spring AI의 TextReader로 마크다운 파일 읽기
+            TextReader reader = new TextReader(resource);
+
+            // 2. 파일 내용을 문서(Document) 리스트로 변환
+            var documents = reader.get();
+
+            // 3.ChromaDB에 저장(이때 OpenAI 임메딩 모델 작동)
+            vectorStore.add(documents);
+
+            System.out.println("파일 주입 완료" + resource.getFilename());
+        }
+    }
 }
--- src/main/java/dev/rag/service/RagService.java
+++ src/main/java/dev/rag/service/RagService.java
@@ -1,6 +1,37 @@
 package dev.rag.service;
 
+import dev.rag.dto.ChatResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.stereotype.Service;
+
 // 사용자의 질문을 분석해 ChromaDB에서 가장 유사한 경험을 찾음(Retrieval)
 // 찾은 정보를 OpenAI 프롬프트에 합쳐서 답변 생성(Generation)
+@Service
+@RequiredArgsConstructor
 public class RagService {
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    /*
+    * 사용자의 질문을 받고 ChromaDB에서 관련 정보를 찾고 AI답변 생성
+    * */
+    public ChatResponse askQuestion(String message) {
+        // 1. ChatClient를 통해 대화 시작
+        String answer = chatClient.prompt()
+                // 2. QuestionAnswerAdvisor : Rag핵심 모듈
+                // -> 질문 벡터화 -> DB검색 -> 컨텍스트 추출 -> 프롬프트 합성
+                .advisors(QuestionAnswerAdvisor.builder(vectorStore)
+                        .searchRequest(SearchRequest.builder().build())
+                        .build())
+                .user(message)
+                .call()
+                .content();
+        return ChatResponse.builder()
+                .answer(answer)
+                .build();
+    }
 }
